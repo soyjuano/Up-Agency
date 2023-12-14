641309c1157358da552295982e94a9bf4001d310
soyjuano <127443846+soyjuano@users.noreply.github.com>
2023-12-14 17:49:17 -0300

    creacion menu y secciones

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+// imagenes
+import Logo from '../assets/logo.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-8'>
+   <div className='container mx-auto'>
+    <div className='flex justify-between items-center'>
+    {/*   logo */}
+    <a href='#'>
+      <img src={Logo} alt='logo' />
+    </a>
+    {/* boton */}
+    <button className='btn btn-sm'>Contáctenos</button>
+    </div>
+    
+    </div>
+    
+    </header>;
 };
 
 export default Header;
