f647ebb621e1364b9e9256ab2004d67b8bcd3f61
soyjuano <127443846+soyjuano@users.noreply.github.com>
2023-12-21 17:56:13 -0300

    imagenes definitivas seccion "servicios"

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,14 +4,14 @@ import { motion } from 'framer-motion';
 // variantes
 import { fadeIn } from '../variants';
 // imagenes
-import Img1 from '../assets/portfolio-img1.png';
-import Img2 from '../assets/portfolio-img2.png';
-import Img3 from '../assets/portfolio-img3.png';
+import Img1 from '../assets/mimi.jpg';
+import Img2 from '../assets/Barbie-Velez.jpg';
+import Img3 from '../assets/el-apache-ness.jpg';
 
 const Work = () => {
   return (
     <section className='section' id='work'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto mt-16 lg:mt-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
           <motion.div 
            variants={fadeIn('right', 0.3)}
@@ -41,15 +41,15 @@ const Work = () => {
               transition-all duration-300'></div>
                 {/* img */}
                 <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt='' />
-                {/* subtitulo */}
+                {/* titulo */}
                 <div className='absolute -bottom-full left-12 group-hover:bottom-24 
                 transition-all duration-500 z-50'>
-                 <span className='text-gradient'>Mimi</span> 
+                 <span className='text-gradient text-3xl'>Mimi</span> 
                   </div>
-                {/* titulo */}
+                {/* subtitulo */}
                 <div className='absolute -bottom-full left-12 group-hover:bottom-14 
                 transition-all duration-700 z-50'>
-                  <span className='text-3x1 text-white'>Panelista</span>
+                  <span className='text-3xl text-white'>Panelista</span>
                 </div>
               </div>
 
@@ -68,15 +68,15 @@ const Work = () => {
               transition-all duration-300'></div>
                 {/* img */}
                 <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt='' />
-                {/* subtitulo */}
+                {/* titulo */}
                 <div className='absolute -bottom-full left-12 group-hover:bottom-24 
                 transition-all duration-500 z-50'>
-                 <span className='text-gradient'>Modelo & Actriz</span> 
+                 <span className='text-gradient text-3xl'>Barbie Velez</span> 
                   </div>
-                {/* titulo */}
+                {/* subtitulo */}
                 <div className='absolute -bottom-full left-12 group-hover:bottom-14 
                 transition-all duration-700 z-50'>
-                  <span className='text-3x1 text-white'>Barbie Velez</span>
+                  <span className='text-3xl text-white'>Modelo & Actriz</span>
                 </div>
               </div>
 
@@ -87,15 +87,15 @@ const Work = () => {
               transition-all duration-300'></div>
                 {/* img */}
                 <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt='' />
-                {/* subtitulo */}
+                {/* titulo */}
                 <div className='absolute -bottom-full left-12 group-hover:bottom-24 
                 transition-all duration-500 z-50'>
-                 <span className='text-gradient'>Cantante</span> 
+                 <span className='text-gradient text-3xl'>Apache Ness</span> 
                   </div>
-                {/* titulo */}
+                {/* subtitulo */}
                 <div className='absolute -bottom-full left-12 group-hover:bottom-14 
                 transition-all duration-700 z-50'>
-                  <span className='text-3x1 text-white'>Apache Ness</span>
+                  <span className='text-3xl text-white'>Cantante</span>
                 </div>
               </div>
 
