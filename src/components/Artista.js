3095a78e03147cc371ed85ea579bb81d7e156536
soyjuano <127443846+soyjuano@users.noreply.github.com>
2024-01-07 20:40:32 -0300

    h2 y parrafo de seccion artistas + armado de grid

--- src/components/Artista.js
+++ src/components/Artista.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Artista = (props) => {
-
-const { url, author, title, urlToImage, content} = props.artista;
+  const { url, author, title, urlToImage, content } = props.artista;
 
   return (
-    <div className='col-md-4'>
-      <div className='card'>
-        <img src={urlToImage} alt={title} className='card-img-top' />
-        <span className='card-title'>{author} </span>
-        <div className='card-body'>
-        <h5 className='card-title'>{title} </h5>
-        <p className='card-text'>{content} </p>
-        <a href={url} className='btn'>Leer mas</a>
+    <div className=''>
+      <div className='card border-2 border-white/20 rounded-xl relative'>
+        <img
+          src={urlToImage}
+          alt={title}
+          className='card-img-top rounded-xl w-full h-[200px] lg:h-[300px] xl:h-[400px]'
+        />
+        <div className='absolute bottom-0 left-0 right-0 bg-black/50 p-2 text-center text-white text-lg'>
+          {author}
         </div>
+        {/* <div className='card-body'>
+          <h5 className='card-title'>{title} </h5>
+          <p className='card-text'>{content} </p>
+          <a href={url} className='btn'>
+            Leer mas
+          </a>
+        </div> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Artista;
--- src/components/Artistas.js
+++ src/components/Artistas.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import Artista from './Artista';
+import { BiSearch } from "react-icons/bi";
+import { BiExpandVertical } from "react-icons/bi";
+// motion
+import { motion } from 'framer-motion';
+// variantes
+import { fadeIn } from '../variants';
 
 class Artistas extends Component {
   state = {
@@ -16,7 +22,7 @@ class Artistas extends Component {
     fetch(url)
       .then((respuesta) => respuesta.json())
       .then((data) => {
-          this.setState({
+        this.setState({
           artistas: data.articles,
         });
       });
@@ -24,15 +30,74 @@ class Artistas extends Component {
 
   render() {
     return (
-      <div id='artistas' className='container mx-auto pt-10'>
-        <h2 className='h2 text-secondary flex flex-col text-center'>Artistas para contratar</h2>
-        <div className='grid grid-cols-3 gap-4'>
-        {this.state.artistas.map((artista) => (
-          <Artista 
-          key={artista.url} 
-          artista={artista} />
-        ))}
+      <div id='artistas' className='container mx-auto sm:pt-5 lg:pt-10'>
+
+        <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className='flex-1 flex flex-col gap-y-5'>
+
+          <h2 className='h2 text-secondary flex flex-col text-center'>Artistas para contratar</h2>
+          <p className='min-w-sm xl:mb-8 mb-2 xl:text-[20px] text-center'>
+            Contrate a artistas destacados en UP! Agency y asegúrese el éxito en sus eventos y fiestas.
+          </p>
+
+          <div className='mx-auto flex flex-col md:flex-row justify-center items-center gap-5 pb-8'>
+
+            {/* buscador por descripcion */}
+            <label class="relative block w-60">
+              <span class="sr-only">Search</span>
+              <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg class="h-5 w-5 fill-slate-900" viewBox="0 0 20 20"><BiSearch /></svg>
+              </span>
+              <input class=" placeholder:text-white block
+           bg-black/20 w-full border border-accent rounded-md py-2 pl-9 pr-3 
+           shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+           focus:ring-1 text-sm"
+                placeholder="Encuentre su Artista favorito"
+                type="text"
+                name="search" />
+            </label>
+            {/* buscador por categorias */}
+            <label className="relative block w-60">
+              <span className="sr-only">Search category</span>
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg className="h-5 w-5 fill-slate-900" viewBox="0 0 20 20">
+                  {/* Icono para el despliegue */}
+                </svg>
+              </span>
+              <select
+                className="block bg-black/20 w-full border border-accent rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm placeholder-text-slate-400"
+                name="categorias"
+              >
+                <option value="" disabled selected>
+                  Selecciona una categoría
+                </option>
+                <option value="categoria1">Categoría 1</option>
+                <option value="categoria2">Categoría 2</option>
+                <option value="categoria3">Categoría 3</option>
+                {/* Agrega más opciones según tus necesidades */}
+              </select>
+            </label>
+          </div>
+        </motion.div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8'>
+          {this.state.artistas.map((artista) => (
+            <motion.div
+              key={artista.url}
+              variants={fadeIn('left', 0.2)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.3 }}
+              className='flex-1 flex flex-col lg:mb-0'>
+              <Artista artista={artista} />
+            </motion.div>
+          ))}
         </div>
+
       </div>
     );
   }
